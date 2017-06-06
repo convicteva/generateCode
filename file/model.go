ebc8ea0e164e96992da19f2eb1651a91f4bb3916
masongzhi <maszj921@126.com>
2017-06-06 15:39:23 +0800

    生成get set 方法

--- file/model.go
+++ file/model.go
@@ -22,6 +22,7 @@ func GenerateBaseModel(filepath, packageName string) {
 	//列定义
 	columnSlice := make([]db.Column, 0, 4)
 	columnSlice = append(columnSlice, db.Column{"id", "", "BIGINT"})
+	columnSlice = append(columnSlice, db.Column{"order_id", "订单id", "BIGINT"})
 	columnSlice = append(columnSlice, db.Column{"remark", "", "VARCHAR"})
 	columnSlice = append(columnSlice, db.Column{"create_time", "", "TIMESTAMP"})
 	columnSlice = append(columnSlice, db.Column{"status", "", "INT"})
@@ -43,12 +44,22 @@ func GenerateBaseModel(filepath, packageName string) {
 	}
 
 	fmt.Fprintln(w, "public class BaseModel implements Serializable {")
+	fmt.Fprintln(w, "")
 
 	//属性
-	propertiesSlice := generateJavaPropertiesByColumn(propertySlice)
+	propertiesSlice := generateJavaPropertiesByProperties(propertySlice)
 	if len(propertiesSlice) > 0 {
 		for _, v := range propertiesSlice {
 			fmt.Fprintln(w, v)
+		}
+	}
+	fmt.Fprintln(w, "")
+
+	//get set 方法
+	getSetFuncSlice := generateGeterSeterFuncByProperties(propertySlice)
+	if len(getSetFuncSlice) > 0 {
+		for _, s := range getSetFuncSlice {
+			fmt.Fprintln(w, s)
 		}
 	}
 
@@ -73,28 +84,40 @@ func generateImportByProperties(properties []db.JavaProperty) []string {
 /**
 根据列生成java属性代码
 */
-func generateJavaPropertiesByColumn(properties []db.JavaProperty) []string {
-	propertiesSlice := make([]string, 0, 10)
+func generateJavaPropertiesByProperties(properties []db.JavaProperty) []string {
+	propertiesSlice := make([]string, 0, len(properties)*2)
 	for _, p := range properties {
+		if !strings.EqualFold("", p.Comment) {
+			propertiesSlice = append(propertiesSlice, javaCodeRetractSpace_1+"/** "+p.Comment+" */")
+		}
 		propertiesSlice = append(propertiesSlice, javaCodeRetractSpace_1+"private "+p.DataType+" "+p.Name)
+		propertiesSlice = append(propertiesSlice, "")
 	}
 	return propertiesSlice
 }
 
-type javaProperty struct {
-	//字段名
-	Name string
-	//注释
-	Comment string
-	//类型
-	DataType string
-}
-
 /**
 根据列信息生成get set 方法
+返回插入信息
 */
-func generateGeterSeterFuncByColumn(propertiesSlice []string) []string {
-	return nil
+func generateGeterSeterFuncByProperties(propertiesSlice []db.JavaProperty) []string {
+	getSetFunSlice := make([]string, 0, len(propertiesSlice)*8)
+	for _, p := range propertiesSlice {
+		//get 方法
+		getSetFunSlice = append(getSetFunSlice, javaCodeRetractSpace_1+"public "+p.DataType+" "+"get"+strings.Title(p.Name)+"(){")
+		getSetFunSlice = append(getSetFunSlice, javaCodeRetractSpace_2+"return this."+p.Name)
+		getSetFunSlice = append(getSetFunSlice, javaCodeRetractSpace_1+"}")
+		getSetFunSlice = append(getSetFunSlice, "")
+
+		//set 方法
+		getSetFunSlice = append(getSetFunSlice, javaCodeRetractSpace_1+"public void "+"set"+strings.Title(p.Name)+"("+p.DataType+" "+p.Name+"){")
+		getSetFunSlice = append(getSetFunSlice, javaCodeRetractSpace_2+"this."+p.Name+" = "+p.Name)
+		getSetFunSlice = append(getSetFunSlice, javaCodeRetractSpace_1+"}")
+		getSetFunSlice = append(getSetFunSlice, "")
+
+	}
+
+	return getSetFunSlice
 }
 
 func GenerateMode(packageName, modelName string) {
